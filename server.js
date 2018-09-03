ebed4fc03e72acc2d762ef34ac0b178da242483e
daryllpenafiel <daryll_penafiel@yahoo.com>
2018-09-03 17:04:19 -0400

    trying to deploy to Heroku

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var exphbs = require("express-handlebars");
 var db = require("./models");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+
+// var PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,22 +25,22 @@ require("./routes/html-routes")(app);
 
 var syncOptions = { force: false };
 
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test")
-{
-  syncOptions.force = true;
-}
+// // If running a test, set syncOptions.force to true
+// // clearing the `testdb`
+// if (process.env.NODE_ENV === "test")
+// {
+//   syncOptions.force = true;
+// }
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function ()
 {
-  app.listen(PORT, function ()
+  app.listen(process.env.PORT || 3000, function ()
   {
     console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
+      "==>  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      process.env.PORT || 3000,
+      process.env.PORT || 3000
     );
   });
 });
